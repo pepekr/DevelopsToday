0da65847fc6aab8a525c6aeb519b52c80fe54d63
illia <illiakrang@gmail.com>
2025-07-19 00:05:18 +0300

    feat: add sign in form hook styles

---
+++ client/src/pages/SignIn.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { useSigninForm } from "../hooks/useSignInHook"
+import '../styles/signin.css';
+import { useNavigate } from 'react-router';
+
+export default function Signin({setIsAuthenticated}:{setIsAuthenticated:React.Dispatch<React.SetStateAction<boolean>>}) {
+  const navigate = useNavigate();
+  const [signinError, setSigninError] = useState<string>("");
+
+  const {
+    formData,
+    errors,
+    handleInputChange,
+    handleSubmit,
+  } = useSigninForm();
+
+  const onSubmit = async (data: any) => {
+    const res = await fetch(`${process.env.REACT_APP_BACKEND_API_URL}/signin`, {
+      headers: {
+        "Content-type": "application/json",
+      },
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+      const error = await res.json();
+      setSigninError(error.message || "Signin failed");
+    } else {
+      navigate('/')
+      setIsAuthenticated(true)
+    }
+  };
+
+  return (
+    <div className="signin-container">
+      <h2>Create Account</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="signin-group">
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            placeholder="john@example.com"
+          />
+          {errors.email && <div className="signin-error">{errors.email}</div>}
+        </div>
+
+        <div className="signin-group">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            placeholder="********"
+          />
+          {errors.password && <div className="signin-error">{errors.password}</div>}
+        </div>
+
+        <div className="signin-group">
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            name="confirmedPassword"
+            value={formData.confirmedPassword}
+            onChange={handleInputChange}
+            placeholder="********"
+          />
+          {errors.confirmedPassword && <div className="signin-error">{errors.confirmedPassword}</div>}
+        </div>
+
+        <button className="signin-button" type="submit">
+          Sign Up
+        </button>
+        <button onClick= {()=>navigate("/login")}className="signin-button" type="submit">
+          Log in
+        </button>
+        {signinError && <div className="signin-error">{signinError}</div>}
+      </form>
+    </div>
+  );
+}
